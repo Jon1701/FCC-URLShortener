7c1e65a69d7414c32da10c92389ea13ac8db14d9
Jon Balon <development@jonbalon.com>
2016-09-07 20:03:35 -0400

    Added url length input check

--- src/js/script.js
+++ src/js/script.js
@@ -16,45 +16,67 @@ $(document).ready(function() {
     // Prevent default action.
     e.preventDefault();
 
-    // Get the URL entered by the user.
-    var url = $('#input-url').val();
+    // Get the URL entered by the user, and trim
+    // leading and following whitespace.
+    var url = $('#input-url').val().trim();
 
-    // Make an ajax request to the server
-    // at the /new endpoint.
-    var jqxhr = $.getJSON('/new/' + url);
+    // Check to see if the input URL is empty.
+    if (url.length == 0) {
 
-    // Once the request is done, parse the response.
-    jqxhr.done(function(response) {
+      // If the input URL is empty, show an error to the user
+      // via modal.
+      var msg = 'Please enter a URL.';
 
-      // Check to see if the response from the server returned some kind of
-      // error.
-      if (response.hasOwnProperty('error')) {
+      // Send error message to modal.
+      $('#modal-error .message').text(msg);
 
-        // If an error occured, display an error message to the user.
-        var msg = response['error']['msg'];
+      // Open the modal dialog.
+      $('#modal-error').openModal();
 
-        // Send error message to modal.
-        $('#modal-error .message').text(msg);
+    } else {
 
-        // Open the modal dialog.
-        $('#modal-error').openModal();
 
-      } else {
+      // Make an ajax request to the server
+      // at the /new endpoint.
+      var jqxhr = $.getJSON('/new/' + url);
 
-        // Obtain the short URL.
-        var shortUrl = response['short_url'];
+      // Once the request is done, parse the response.
+      jqxhr.done(function(response) {
 
-        // Send short URL to modal.
-        $('#modal-success #field_short_url').val(shortUrl);
+        // Check to see if the response from the server returned some kind of
+        // error.
+        if (response.hasOwnProperty('error')) {
 
-        // Open the modal dialog.
-        $('#modal-success').openModal();
+          // If an error occured, display an error message to the user.
+          var msg = response['error']['msg'];
 
-      }
+          // Send error message to modal.
+          $('#modal-error .message').text(msg);
 
-    });
+          // Open the modal dialog.
+          $('#modal-error').openModal();
 
-  });
+        } else {
+
+          // Obtain the short URL.
+          var shortUrl = response['short_url'];
+
+          // Send short URL to modal.
+          $('#modal-success #field_short_url').val(shortUrl);
+
+          // Open the modal dialog.
+          $('#modal-success').openModal();
+
+          // Clear URL input.
+          $('#input-url').val('');
+
+        };
+
+      });
+
+    }; // End URL length check.
+
+  }); // End URL form submission.
 
 
 });
